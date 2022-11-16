e8a35b8e683db5b9a954bf472cf6d0c098252dd9
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-11-16 13:04:16 +0530

    CSS in JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
-import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+import "./App.css";
 
 const PokemonRow = ({ poke, onSelect }) => {
 	return (
@@ -54,6 +56,25 @@ PokemonInfo.propTypes = {
 	}),
 };
 
+const Title = styled.h1`
+	text-align: center;
+`;
+const TwoColumnLayout = styled.div`
+	display: grid;
+	grid-template-columns: 70% 30%;
+	grid-column-gap: "1rem";
+`;
+const Container = styled.div`
+	margin: auto;
+	width: 800px;
+	padding-top: 1rem;
+`;
+const Input = styled.input`
+	width: 100%;
+	font-size: x-large;
+	padding: 0.2rem;
+`;
+
 function App() {
 	const [filter, setFilter] = useState("");
 	const [selectedItem, setSelectedItem] = useState(null);
@@ -69,19 +90,14 @@ function App() {
 	}, []);
 
 	return (
-		<div style={{ margin: "auto", width: 800, paddingTop: "1rem" }}>
-			<h1 className="title">Pokemon Search</h1>
-			<input
+		<Container>
+			<Title>Pokemon Search</Title>
+			<Input
 				type="text"
 				value={filter}
 				onChange={(e) => setFilter(e.target.value)}
 			/>
-			<div
-				style={{
-					display: "grid",
-					gridTemplateColumns: "70% 30%",
-					gridColumnGap: "1rem",
-				}}>
+			<TwoColumnLayout>
 				<div className="">
 					<table width="100%">
 						<thead>
@@ -113,8 +129,8 @@ function App() {
 						<PokemonInfo {...selectedItem} />
 					</div>
 				)}
-			</div>
-		</div>
+			</TwoColumnLayout>
+		</Container>
 	);
 }
 
